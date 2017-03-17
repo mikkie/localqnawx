163854886d74a515b50b51bb704a64c27d00df5d
mikkie <lmj3648xm@hotmail.com>
2017-03-17 21:24:00 +0800

    pick pic

--- pages/newTopic/newTopic.js
+++ pages/newTopic/newTopic.js
@@ -10,7 +10,8 @@ Page({
     content : '',
     expireLengths : ['5','1','4','24','7'],
     expireDateUnits : ['m','h','h','h','d'],
-    anonymous : false
+    anonymous : false,
+    photoes : []
   },
   confirmContent : function(e){
      this.data.content = e.detail.value;
@@ -59,5 +60,38 @@ Page({
   },
   onLoad : function(options){
        this.setData({currentLoc : options.curLocl,communityId : options.communityId});
+  },
+  pickPic : function(){
+    var that = this;
+    wx.chooseImage({
+      count: 9, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+         // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+         if(that.data.photoes.length < 9){
+            if(res.tempFilePaths && res.tempFilePaths.length > 0){
+               for(var i in res.tempFilePaths){
+                  that.data.photoes.push(res.tempFilePaths[i]);
+               } 
+               that.setData({photoes : that.data.photoes});
+            }
+         }
+      }
+    });
+  },
+  previewPic : function(e){
+    var that = this;
+    var index = e.currentTarget.dataset.index;
+    if(that.data.photoes[index]){
+      wx.previewImage({
+        urls: [that.data.photoes[index]] 
+      });
+    }
+  },
+  removePic : function(e){
+    var index = e.currentTarget.dataset.index;
+    this.data.photoes.splice(index,1);
+    this.setData({photoes : this.data.photoes});
   } 
 })
