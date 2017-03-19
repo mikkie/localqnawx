ecbe76d83dcbc5eaa1ca5b1dc21d9724f4cd6614
mikkie <lmj3648xm@hotmail.com>
2017-03-19 12:59:04 +0800

    network

--- pages/newTopic/newTopic.js
+++ pages/newTopic/newTopic.js
@@ -84,25 +84,22 @@ Page({
      }
   },
   onLoad : function(options){
-       this.setData({currentLoc : options.curLocl,communityId : options.communityId});
+       this.setData({currentLoc : options.curLocl,communityId : options.communityId,photoes : []});
        this.initUpload();
   },
   pickPic : function(){
     var that = this;
     wx.chooseImage({
-      count: 9, // 默认9
+      count: 9 - that.data.photoes.length, // 默认9
       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
       success: function (res) {
-         // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-         if(that.data.photoes.length < 9){
-            if(res.tempFilePaths && res.tempFilePaths.length > 0){
-               for(var i in res.tempFilePaths){
-                  that.data.photoes.push(res.tempFilePaths[i]);
-               } 
-               that.setData({photoes : that.data.photoes});
-            }
-         }
+        if(res.tempFilePaths && res.tempFilePaths.length > 0){
+          for(var i in res.tempFilePaths){
+            that.data.photoes.push(res.tempFilePaths[i]);
+          } 
+          that.setData({photoes : that.data.photoes});
+        }
       }
     });
   },
